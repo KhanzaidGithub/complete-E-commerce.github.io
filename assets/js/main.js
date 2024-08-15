d620c3cbf4be3354494afb2f340951328af318b9
KhanzaidGithub <zaidscestudent@gmail.com>
2024-08-15 11:06:21 -0700

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,18 @@ var swiper = new Swiper(".mySwiper", {
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('active-tab');
+        });
+        target.classList.add('active-tab');
+        tabs.forEach((tab) => {
+            tab.classList.remove('active-tab');
+        });
+        tab.classList.add('active-tab');
+    });
+});
